6d4b153e1f2bc413ffd45077446cb14377eeb683
josephyne03 <josephyne03@gmail.com>
2021-05-29 06:08:28 +0000

    3

--- api/one.ts
+++ api/one.ts
@@ -1,4 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+const fetch = require("node-fetch")
 
 export default function one(req: VercelRequest, res: VercelResponse) {
     fetch("https://website-with-api.vercel.app/api/one").then(() => {
